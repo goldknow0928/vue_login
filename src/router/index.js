0f701fb6db547e7fb6d9159f0fc670cc0cc75a40
김아라 <goldknow456@gmail.com>
2021-05-18 18:24:17 +0900

    axios 테스트

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+//import Home from "../views/Home.vue";
+import Test from "../views/Test.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Test",
+    component: Test,
   },
+  // {
+  //   path: "/",
+  //   name: "Home",
+  //   component: Home,
+  // },
   {
     path: "/about",
     name: "About",
