4f6b4c32824339a84ca253e16e5f3706c9563d9a
김아라 <goldknow456@gmail.com>
2021-05-23 15:26:46 +0900

    localstorage 사용

--- src/main.js
+++ src/main.js
@@ -10,5 +10,9 @@ new Vue({
   router,
   store,
   vuetify,
+  beforeCreate() {
+    //새로고침시 토큰으로 멤버정보 받기
+    this.$store.dispatch("getMemberInfo");
+  },
   render: (h) => h(App),
 }).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -48,35 +48,18 @@ export default new Vuex.Store({
   },
   actions: {
     //로그인 시도
-    login({ commit }, loginObj) {
+    login({ dispatch }, loginObj) {
+      //1. 실제 로그인 -> 토큰 반환
       axios
         .post("https://reqres.in/api/login", loginObj) //파라메터(body)
         .then((res) => {
           //성공 시 token: 블라블라(실제로는 user_id값을 받는다)
           //토큰을 헤더에 포함시켜서 유저 정보를 요청
-          let token = res.data.token; //axios 스키마에서 토큰을 보낸다
-          let config = {
-            headers: {
-              "access-token": token,
-            },
-          };
-          axios
-            .get("https://reqres.in/api/user/2", config) //config를 헤더에 보낸다
-            .then((response) => {
-              let userInfo = {
-                color: response.data.data.color,
-                id: response.data.data.id,
-                name: response.data.data.name,
-                pantone_value: response.data.data.pantone_value,
-                year: response.data.data.year,
-              };
-              commit("loginSuccess", userInfo);
-              //commit('loginSuccess', response.data.data)
-              // => userInfo의 모든 정보를 보낼때 위와 같이 써도 되지만, 명세를 해주면 다른 개발자들과 협업을 할 때 더 보기 좋기 때문에 전자와 같이 하는게 좋다.
-            })
-            .catch(() => {
-              alert("이메일과 비밀번호를 확인하세요.");
-            });
+
+          let token = res.data.token;
+          //토큰을 로컬스토리지에 저장
+          localStorage.setItem("access_token", token);
+          dispatch("getMemberInfo");
         })
         .catch(() => {
           alert("이메일과 비밀번호를 확인하세요.");
@@ -88,6 +71,36 @@ export default new Vuex.Store({
       router.push({
         name: "home",
       }); //로그아웃시 홈으로 이동
+    },
+
+    getMemberInfo({ commit }) {
+      //로컬스토리지에 있는 토큰을 불러온다
+      let token = localStorage.getItem("access_token");
+      let config = {
+        headers: {
+          "access-token": token,
+        },
+      };
+
+      //2. 토큰 -> 멤버 정보를 반환
+      //! 새로고침 -> 토큰만 가지고 멤버정보를 요청
+      axios
+        .get("https://reqres.in/api/user/2", config) //config를 헤더에 보낸다
+        .then((response) => {
+          let userInfo = {
+            color: response.data.data.color,
+            id: response.data.data.id,
+            name: response.data.data.name,
+            pantone_value: response.data.data.pantone_value,
+            year: response.data.data.year,
+          };
+          commit("loginSuccess", userInfo);
+          //commit('loginSuccess', response.data.data)
+          // => userInfo의 모든 정보를 보낼때 위와 같이 써도 되지만, 명세를 해주면 다른 개발자들과 협업을 할 때 더 보기 좋기 때문에 전자와 같이 하는게 좋다.
+        })
+        .catch(() => {
+          alert("이메일과 비밀번호를 확인하세요.");
+        });
     },
   },
   modules: {},
